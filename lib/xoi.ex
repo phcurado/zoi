661568d869a86799d0279bc74696dd7aeca33be1
Paulo Curado <phcurado@gmail.com>
2025-07-24 15:18:17 -0400

    mix new

---
+++ lib/xoi.ex
@@ -0,0 +1,18 @@
+defmodule Xoi do
+  @moduledoc """
+  Documentation for `Xoi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Xoi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/xoi_test.exs
@@ -0,0 +1,8 @@
+defmodule XoiTest do
+  use ExUnit.Case
+  doctest Xoi
+
+  test "greets the world" do
+    assert Xoi.hello() == :world
+  end
+end
