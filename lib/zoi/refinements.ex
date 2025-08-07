1a385e2c2dd11ffa707173f3138ccb4f8c9f2a47
Paulo Curado <phcurado@gmail.com>
2025-08-07 21:17:36 +0300

    added uuid format

--- lib/zoi/refinements.ex
+++ lib/zoi/refinements.ex
@@ -9,139 +9,139 @@ defmodule Zoi.Refinements do
     end
   end
 
-  def refine(%Zoi.Types.Integer{}, input, [min: min], _opts) do
+  def refine(%Zoi.Types.Integer{}, input, [min: min], opts) do
     if input >= min do
       :ok
     else
-      {:error, "too small: must be at least #{min}"}
+      {:error, message(opts, "too small: must be at least #{min}")}
     end
   end
 
-  def refine(%Zoi.Types.Float{}, input, [min: min], _opts) do
+  def refine(%Zoi.Types.Float{}, input, [min: min], opts) do
     if input >= min do
       :ok
     else
-      {:error, "too small: must be at least #{min}"}
+      {:error, message(opts, "too small: must be at least #{min}")}
     end
   end
 
-  def refine(%Zoi.Types.Array{}, input, [min: min], _opts) do
+  def refine(%Zoi.Types.Array{}, input, [min: min], opts) do
     if length(input) >= min do
       :ok
     else
-      {:error, "too small: must have at least #{min} items"}
+      {:error, message(opts, "too small: must have at least #{min} items")}
     end
   end
 
-  def refine(%Zoi.Types.String{}, input, [gt: gt], _opts) do
+  def refine(%Zoi.Types.String{}, input, [gt: gt], opts) do
     if String.length(input) > gt do
       :ok
     else
-      {:error, "too small: must have more than #{gt} characters"}
+      {:error, message(opts, "too small: must have more than #{gt} characters")}
     end
   end
 
-  def refine(%Zoi.Types.Integer{}, input, [gt: gt], _opts) do
+  def refine(%Zoi.Types.Integer{}, input, [gt: gt], opts) do
     if input > gt do
       :ok
     else
-      {:error, "too small: must be greater than #{gt}"}
+      {:error, message(opts, "too small: must be greater than #{gt}")}
     end
   end
 
-  def refine(%Zoi.Types.Float{}, input, [gt: gt], _opts) do
+  def refine(%Zoi.Types.Float{}, input, [gt: gt], opts) do
     if input > gt do
       :ok
     else
-      {:error, "too small: must be greater than #{gt}"}
+      {:error, message(opts, "too small: must be greater than #{gt}")}
     end
   end
 
-  def refine(%Zoi.Types.Array{}, input, [gt: gt], _opts) do
+  def refine(%Zoi.Types.Array{}, input, [gt: gt], opts) do
     if length(input) > gt do
       :ok
     else
-      {:error, "too small: must have more than #{gt} items"}
+      {:error, message(opts, "too small: must have more than #{gt} items")}
     end
   end
 
-  def refine(%Zoi.Types.String{}, input, [max: max], _opts) do
+  def refine(%Zoi.Types.String{}, input, [max: max], opts) do
     if String.length(input) <= max do
       :ok
     else
-      {:error, "too big: must have at most #{max} characters"}
+      {:error, message(opts, "too big: must have at most #{max} characters")}
     end
   end
 
-  def refine(%Zoi.Types.Integer{}, input, [max: max], _opts) do
+  def refine(%Zoi.Types.Integer{}, input, [max: max], opts) do
     if input <= max do
       :ok
     else
-      {:error, "too big: must be at most #{max}"}
+      {:error, message(opts, "too big: must be at most #{max}")}
     end
   end
 
-  def refine(%Zoi.Types.Float{}, input, [max: max], _opts) do
+  def refine(%Zoi.Types.Float{}, input, [max: max], opts) do
     if input <= max do
       :ok
     else
-      {:error, "too big: must be at most #{max}"}
+      {:error, message(opts, "too big: must be at most #{max}")}
     end
   end
 
-  def refine(%Zoi.Types.Array{}, input, [max: max], _opts) do
+  def refine(%Zoi.Types.Array{}, input, [max: max], opts) do
     if length(input) <= max do
       :ok
     else
-      {:error, "too big: must have at most #{max} items"}
+      {:error, message(opts, "too big: must have at most #{max} items")}
     end
   end
 
-  def refine(%Zoi.Types.String{}, input, [lt: lt], _opts) do
+  def refine(%Zoi.Types.String{}, input, [lt: lt], opts) do
     if String.length(input) < lt do
       :ok
     else
-      {:error, "too big: must have less than #{lt} characters"}
+      {:error, message(opts, "too big: must have less than #{lt} characters")}
     end
   end
 
-  def refine(%Zoi.Types.Integer{}, input, [lt: lt], _opts) do
+  def refine(%Zoi.Types.Integer{}, input, [lt: lt], opts) do
     if input < lt do
       :ok
     else
-      {:error, "too big: must be less than #{lt}"}
+      {:error, message(opts, "too big: must be less than #{lt}")}
     end
   end
 
-  def refine(%Zoi.Types.Float{}, input, [lt: lt], _opts) do
+  def refine(%Zoi.Types.Float{}, input, [lt: lt], opts) do
     if input < lt do
       :ok
     else
-      {:error, "too big: must be less than #{lt}"}
+      {:error, message(opts, "too big: must be less than #{lt}")}
     end
   end
 
-  def refine(%Zoi.Types.Array{}, input, [lt: lt], _opts) do
+  def refine(%Zoi.Types.Array{}, input, [lt: lt], opts) do
     if length(input) < lt do
       :ok
     else
-      {:error, "too big: must have less than #{lt} items"}
+      {:error, message(opts, "too big: must have less than #{lt} items")}
     end
   end
 
-  def refine(%Zoi.Types.String{}, input, [length: length], _opts) do
+  def refine(%Zoi.Types.String{}, input, [length: length], opts) do
     if String.length(input) == length do
       :ok
     else
-      {:error, "invalid length: must have #{length} characters"}
+      {:error, message(opts, "invalid length: must have #{length} characters")}
     end
   end
 
-  def refine(%Zoi.Types.Array{}, input, [length: length], _opts) do
+  def refine(%Zoi.Types.Array{}, input, [length: length], opts) do
     if length(input) == length do
       :ok
     else
-      {:error, "invalid length: must have #{length} items"}
+      {:error, message(opts, "invalid length: must have #{length} items")}
     end
   end
 
@@ -153,19 +153,19 @@ defmodule Zoi.Refinements do
     end
   end
 
-  def refine(%Zoi.Types.String{}, input, [starts_with: prefix], _opts) do
+  def refine(%Zoi.Types.String{}, input, [starts_with: prefix], opts) do
     if String.starts_with?(input, prefix) do
       :ok
     else
-      {:error, "invalid string: must start with '#{prefix}'"}
+      {:error, message(opts, "invalid string: must start with '#{prefix}'")}
     end
   end
 
-  def refine(%Zoi.Types.String{}, input, [ends_with: suffix], _opts) do
+  def refine(%Zoi.Types.String{}, input, [ends_with: suffix], opts) do
     if String.ends_with?(input, suffix) do
       :ok
     else
-      {:error, "invalid string: must end with '#{suffix}'"}
+      {:error, message(opts, "invalid string: must end with '#{suffix}'")}
     end
   end
 
--- lib/zoi/regexes.ex
+++ lib/zoi/regexes.ex
@@ -8,6 +8,34 @@ defmodule Zoi.Regexes do
   def email, do: @email
 
   @doc """
+  Regex pattern to match a valid UUID.
+  """
+  @uuid ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-8][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+  @uuid_v1 ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+  @uuid_v2 ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[2][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+  @uuid_v3 ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[3][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+  @uuid_v4 ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[4][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+  @uuid_v5 ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+  @uuid_v6 ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[6][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+  @uuid_v7 ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[7][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+  @uuid_v8 ~r/^([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[8][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12})$/
+
+  def uuid(opts \\ []) do
+    case opts[:version] do
+      "v1" -> @uuid_v1
+      "v2" -> @uuid_v2
+      "v3" -> @uuid_v3
+      "v4" -> @uuid_v4
+      "v5" -> @uuid_v5
+      "v6" -> @uuid_v6
+      "v7" -> @uuid_v7
+      "v8" -> @uuid_v8
+      nil -> @uuid
+      _ -> raise ArgumentError, "Invalid UUID version: #{opts[:version]}"
+    end
+  end
+
+  @doc """
   Regex pattern to match a valid URL.
   """
   @url ~r/^(https?|mailto):\/\/(([\w-]+\.)+[\w-]+|localhost|127(?:\.\d{1,3}){3})(:\d+)?(\/[\w\-._~:\/?#[\]@!$&'()*+,;=]*)?$/i
