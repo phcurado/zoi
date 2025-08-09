68e1d8cf2e02db674047a823a32907adb65806bf
Paulo Curado <phcurado@gmail.com>
2025-08-09 13:22:38 +0300

    add date as main types with coercion

---
+++ lib/zoi/types/datetime.ex
@@ -0,0 +1,51 @@
+defmodule Zoi.Types.DateTime do
+  @moduledoc false
+  use Zoi.Type.Def, fields: [coerce: false]
+
+  def new(opts \\ []) do
+    apply_type(opts)
+  end
+
+  defimpl Zoi.Type do
+    def parse(_schema, %DateTime{} = input, _opts) do
+      {:ok, input}
+    end
+
+    def parse(schema, input, opts) do
+      coerce = Keyword.get(opts, :coerce, schema.coerce)
+
+      case coerce do
+        true -> coerce(schema, input)
+        _false -> error(schema)
+      end
+    end
+
+    defp coerce(schema, input) when is_binary(input) do
+      case DateTime.from_iso8601(input) do
+        {:error, _reason} ->
+          error(schema)
+
+        {:ok, parsed, _offset} ->
+          {:ok, parsed}
+      end
+    end
+
+    defp coerce(schema, input) when is_integer(input) do
+      case DateTime.from_unix(input) do
+        {:error, _reason} ->
+          error(schema)
+
+        {:ok, parsed} ->
+          {:ok, parsed}
+      end
+    end
+
+    defp coerce(schema, _input) do
+      error(schema)
+    end
+
+    defp error(schema) do
+      {:error, schema.meta.error || "invalid type: must be a datetime"}
+    end
+  end
+end
