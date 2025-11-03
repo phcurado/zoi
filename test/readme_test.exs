8cc10fa7a50f613d58c2b13e7d51ad0dd8f0d5b6
Paulo Curado <phcurado@gmail.com>
2025-11-03 19:52:44 +0200

    deprecate 1.14

--- test/readme_test.exs
+++ test/readme_test.exs
@@ -1,12 +1,9 @@
 defmodule ReadmeTest do
   use ExUnit.Case
-
-  if System.version() == "1.19.2" do
-    doctest_file("README.md")
-    doctest_file("guides/converting_keys_from_object.md")
-    doctest_file("guides/generating_schemas_from_json_example.md")
-    doctest_file("guides/quickstart_guide.md")
-    doctest_file("guides/using_zoi_to_generate_openapi_specs.md")
-    doctest_file("guides/validating_controller_parameters.md")
-  end
+  doctest_file("README.md")
+  doctest_file("guides/converting_keys_from_object.md")
+  doctest_file("guides/generating_schemas_from_json_example.md")
+  doctest_file("guides/quickstart_guide.md")
+  doctest_file("guides/using_zoi_to_generate_openapi_specs.md")
+  doctest_file("guides/validating_controller_parameters.md")
 end
