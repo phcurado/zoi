511d43d4acd6b405d7039af295fe2ab0df90ad61
Paulo Curado <phcurado@gmail.com>
2025-11-10 23:27:19 +0200

    format

--- lib/zoi/struct.ex
+++ lib/zoi/struct.ex
@@ -43,19 +43,18 @@ defmodule Zoi.Struct do
   This is useful for defining `@enforce_keys` in Elixir structs.
   """
   def enforce_keys(%Zoi.Types.Struct{fields: fields}) do
-    Enum.reduce(fields, [], fn
-      {key, type}, acc ->
-        type =
-          case type do
-            %Zoi.Types.Default{inner: inner} -> inner
-            type -> type
-          end
-
-        if Meta.required?(type.meta) do
-          [key | acc]
-        else
-          acc
+    Enum.reduce(fields, [], fn {key, type}, acc ->
+      type =
+        case type do
+          %Zoi.Types.Default{inner: inner} -> inner
+          type -> type
         end
+
+      if Meta.required?(type.meta) do
+        [key | acc]
+      else
+        acc
+      end
     end)
   end
 
